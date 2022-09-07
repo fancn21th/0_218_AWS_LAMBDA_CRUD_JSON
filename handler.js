f0c0e6567b82295fe23a8b73738462600aeb88b1
fancn21th <fancn21th@gmail.com>
2022-09-07 14:12:36 +0800

    esm style

--- handler.js
+++ handler.js
@@ -1,31 +1,12 @@
-"use strict";
-
-const fetch = require("node-fetch");
-const AWS = require("aws-sdk");
-
-const s3 = new AWS.S3();
-
-module.exports.jsonCrud = (event, context, callback) => {
-  fetch(event.image_url)
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      }
-      return Promise.reject(
-        new Error(
-          `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`
-        )
-      );
-    })
-    .then((response) => response.buffer())
-    .then((buffer) =>
-      s3
-        .putObject({
-          Bucket: process.env.BUCKET,
-          Key: event.key,
-          Body: buffer,
-        })
-        .promise()
-    )
-    .then((v) => callback(null, v), callback);
+export const jsonCrud = (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        data: event.data,
+      },
+      null,
+      2
+    ),
+  };
 };
